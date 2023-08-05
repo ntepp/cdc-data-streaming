caf9985bee88205b8f7b1dad8384af59426432d9
ntepp <ntepp.marcus@gmail.com>
2023-08-05 22:48:11 +0200

    Project initialisation

---
+++ src/main/java/com/perinfinity/cdcstreaming/CdcStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.perinfinity.cdcstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcStreamingApplication.class, args);
+	}
+
+}
